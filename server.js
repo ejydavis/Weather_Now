e7f4666d0ada46cc3a93bdb2764bb21486ef49ef
ejydavis <ejesyang@gmail.com>
2019-02-06 19:35:15 -0500

    Queries fix and import of css

---
+++ server.js
@@ -0,0 +1,43 @@
+/*
+  Express for deployment
+  Body parser for parsing for weatherQuery
+  API key used from openweathermap.org
+*/
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = '67f615aee5bd8a92aa50191e57c29320';
+
+/*
+  ejs set to index.ejs found in Views folder
+*/
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine','ejs')
+app.use(express.static('public'));
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  // API call for weather data
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherQuery = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherQuery, error: null});
+        console.log(weather);
+      }
+    }
+  });
+})
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
